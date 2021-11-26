203d75af8bb591780e3c84ff38c75ce47e14f8a5
Peter Vila <peterdvila@gmail.com>
2021-11-26 10:10:47 -0800

    Completed Mini-Max Sum
    Split the given array into two, one that had the smallest integer removed and one that had the highest integer removed. There I used the reduce method on the arrays to add their values.

---
+++ Hackerrank/miniMaxSum.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+    const sorted = arr.sort();
+    const min = sorted.slice(0, -1)
+    const max = sorted.splice(1)
+    const minSum = min.reduce((a, total) => a + total, 0);
+    const maxSum = max.reduce((a, total) => a + total, 0);
+    console.log(minSum, maxSum)
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
