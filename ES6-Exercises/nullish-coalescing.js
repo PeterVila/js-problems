04cf56e5b00370de6c59be56d10e21bd07c8bb42
Peter Vila <peterdvila@gmail.com>
2021-11-27 22:46:24 -0800

    Add files via upload

---
+++ ES6-Exercises/nullish-coalescing.js
@@ -0,0 +1,20 @@
+// The nullish coalescing ?? operator is a new operator introduced in JavaScript that allows you to default to a certain value when the left-hand side is a nullish value.
+
+const getName = name => {
+    return name ?? "N/A";
+}
+
+console.log(getName("Sam")); // "Sam"
+console.log(getName(undefined)); // "N/A"
+console.log(getName(null)); // "N/A"
+
+const getPlaceholder = () => {
+    console.log("getPlaceholder called");
+    return "N/A";
+}
+
+const sayHello = name => {
+    return `Hello ${name ?? getPlaceholder()}`;
+}
+
+console.log(sayHello("Sam")); // "Hello Sam"
