59537af315d07173ff843fddd59ae145fe846597
Peter Vila <peterdvila@gmail.com>
2021-12-02 20:49:35 -0800

    Add files via upload

---
+++ ES6-Exercises/notifications.js
@@ -0,0 +1,13 @@
+import FetchWrapper from './fetch-wrapper.js'
+const checkForNewNotifications = () => {
+    const API = new FetchWrapper("https://jsdemo-3f387-default-rtdb.europe-west1.firebasedatabase.app")
+    API.get("/notifications/new.json").then(data => {
+        showNewNotifications(data.count)
+    })
+}
+
+function showNewNotifications(count) {
+    console.log(`You have ${count} new notifications. Would you like to read them?`);
+}
+
+checkForNewNotifications();
